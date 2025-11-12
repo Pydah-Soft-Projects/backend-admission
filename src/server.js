0e70bee6761d0e7946bb92bfc42e7b98de02fc19
buragaravi <ravi@pydahsoft.in>
2025-11-12 14:15:25 +0530

    sdf

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:3000' || "https://frontend-admission.vercel.app",
+  origin: process.env.CORS_ORIGIN  || "https://frontend-admission.vercel.app" || 'http://localhost:3000',
   credentials: true
 }));
 // Increase JSON payload limit for bulk uploads (50MB)
